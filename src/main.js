13fa12305c3a9cd07945b7eba745e04d300735c2
quinise <quinise.ercolano@gmail.com>
2024-06-06 18:17:00 -0700

    re-organized files for production

--- src/main.js
+++ src/main.js
@@ -1,3 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './stylesheets/about.css';
+import './stylesheets/contact.css';
+import './stylesheets/contactForm.css';
+import './stylesheets/events.css';
+import './stylesheets/gallery.css';
+import './stylesheets/global.css';
+import './stylesheets/home.css';
+import './stylesheets/services.css';
 import PortalVue from 'portal-vue'
 import { createApp } from 'vue'
 import router from '/router.js'
